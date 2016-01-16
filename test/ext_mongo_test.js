c29b6ca75099bfa3a06970dcdfffae5b37bcac88
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-16 20:01:40 +0100

    equality for implicit numerics

--- test/ext_mongo_test.js
+++ test/ext_mongo_test.js
@@ -7,14 +7,25 @@ const parse = eskel.parse
 const ext = eskel.mongo
 
 describe("Mongo Extension", () => {
-  it("equal (string)", () => {
-    let result = parse('name.is.johnny', ext)
-    expect(result.output).to.deep.equal({"name":{"$eq":"johnny"}})
+  describe("Equal", () => {
+    it("equal (string)", () => {
+      let result = parse('name.is.johnny', ext)
+      expect(result.output).to.deep.equal({"name":{"$eq":"johnny"}})
+    })
+    it("not equal (string)", () => {
+      let result = parse('name.is.not.johnny', ext)
+      expect(result.output).to.deep.equal({"name":{"$ne":"johnny"}})
+    })
+    it("equal (implicit numeric)", () => {
+      let result = parse('age.is.33', ext)
+      expect(result.output).to.deep.equal({"age":{"$eq":33}});
+    })
+    it("not equal (implicit numeric)", () => {
+      let result = parse('age.is.not.33', ext)
+      expect(result.output).to.deep.equal({"age":{"$ne":33}});
+    })
   })
-  it("not equal (string)", () => {
-    let result = parse('name.is.not.johnny', ext)
-    expect(result.output).to.deep.equal({"name":{"$ne":"johnny"}})
-  })
+
   it("contain (string", () => {
     let result = parse('name.contains.ohn', ext)
     expect(result.output).to.deep.equal({"name":{"$regex":/ohn/i}})
