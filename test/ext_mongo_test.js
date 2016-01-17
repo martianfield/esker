c0240f9ad7baceb3e9f7838dd350e659cbafb4c5
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-17 11:19:12 +0100

    first shot at logical operators

--- test/ext_mongo_test.js
+++ test/ext_mongo_test.js
@@ -26,29 +26,41 @@ describe("Mongo Extension", () => {
     })
   })
 
-  it("contain (string", () => {
-    let result = parse('name.contains.ohn', ext)
-    expect(result.output).to.deep.equal({"name":{"$regex":/ohn/i}})
-  })
-  it("not contain (string", () => {
-    let result = parse('name.does.not.contain.ohn', ext)
-    expect(result.output).to.deep.equal({"name":{"$regex":/^((?!ohn).)*$/i}})
+  describe("Contain", () => {
+    it("contain (string", () => {
+      let result = parse('name.contains.ohn', ext)
+      expect(result.output).to.deep.equal({"name":{"$regex":/ohn/i}})
+    })
+    it("not contain (string", () => {
+      let result = parse('name.does.not.contain.ohn', ext)
+      expect(result.output).to.deep.equal({"name":{"$regex":/^((?!ohn).)*$/i}})
+    })
   })
 
-  it("greater than (implicit numeric)", () => {
-    let result = parse('age.is.greater.than.12', ext)
-    expect(result.output).to.deep.equal({"age":{"$gt":12}})
+  describe("Greater / less than", () => {
+    it("greater than (implicit numeric)", () => {
+      let result = parse('age.is.greater.than.12', ext)
+      expect(result.output).to.deep.equal({"age":{"$gt":12}})
+    })
+    it("not greater than (implicit numeric)", () => {
+      let result = parse('age.is.not.greater.than.12', ext)
+      expect(result.output).to.deep.equal({"age":{"$lt":12}})
+    })
+    it("less than (implicit numeric)", () => {
+      let result = parse('age.is.smaller.than.12', ext)
+      expect(result.output).to.deep.equal({"age":{"$lt":12}})
+    })
+    it("not less than (implicit numeric)", () => {
+      let result = parse('age.is.not.less.than.12', ext)
+      expect(result.output).to.deep.equal({"age":{"$gt":12}})
+    })
   })
-  it("not greater than (implicit numeric)", () => {
-    let result = parse('age.is.not.greater.than.12', ext)
-    expect(result.output).to.deep.equal({"age":{"$lt":12}})
+
+  describe("Logical Operators", () => {
+    it("or", () => {
+      let result = parse('age.is.20_or_age.is.30', ext)
+      expect(result.output).to.deep.equal({"$or": [{"age":20}, {"age":30}]})
+    })
   })
-  it("less than (implicit numeric)", () => {
-    let result = parse('age.is.smaller.than.12', ext)
-    expect(result.output).to.deep.equal({"age":{"$lt":12}})
-  })
-  it("not less than (implicit numeric)", () => {
-    let result = parse('age.is.not.less.than.12', ext)
-    expect(result.output).to.deep.equal({"age":{"$gt":12}})
-  })
+
 })
